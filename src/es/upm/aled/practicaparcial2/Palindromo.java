873dd47edfbe151336918a63a18de79d0af0e4c2
mariomancebom <mario.mancebo@alumnos.upm.es>
2025-11-16 14:08:34 +0100

    Update Palindromo.java

--- src/es/upm/aled/practicaparcial2/Palindromo.java
+++ src/es/upm/aled/practicaparcial2/Palindromo.java
@@ -2,4 +2,13 @@ package es.upm.aled.practicaparcial2;
 
 public class Palindromo {
 
+	/* Esta clase se basa en hacer en comprobar si una palabra es palindroma, 
+	 * es decir, si se lee igual del derechas que del reves. Para ello, 
+	 * tendremos que hacer un método recursivo, que devuelva true si la 
+	 * palabra es palindroma, y que devuelva false si no lo es
+	 */
+	public static boolean esPalindroma(String palabra) {
+		
+		return true;
+	}
 }
