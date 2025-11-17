655073d4be960793aa5c30dff2297efdd08f9074
mariomancebom <mario.mancebo@alumnos.upm.es>
2025-11-17 19:56:47 +0100

    Create TorreHanoi.java

---
+++ src/es/upm/aled/practicaparcial2/TorreHanoi.java
@@ -0,0 +1,15 @@
+package es.upm.aled.practicaparcial2;
+
+public class TorreHanoi {
+
+	/*
+	 * Mover N discos de la torre A a la torre B, pero solo se pueden 
+	 * mover con las siguientes restricciones:
+	 * - Los discos se mueven de forma individual 
+	 * - No puede haber un disco mas ancho sobre otro menos ancho 
+	 * - Solo podemos mover el disco que se encuentra en la parte mas alta de la torre
+	 */
+	
+	
+	
+}
